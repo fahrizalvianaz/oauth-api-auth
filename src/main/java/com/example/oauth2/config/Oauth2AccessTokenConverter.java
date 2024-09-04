5fee7e021d28e0977d85f5926a0c4a91b37e37b5
WINDOWS <fahrizalshofyanaziz@gmail.com>
2024-09-04 14:02:30 +0700

    add : changes project

--- src/main/java/com/example/oauth2/config/Oauth2AccessTokenConverter.java
+++
@@ -1,4 +0,0 @@
-package com.example.oauth2.config;
-
-public class Oauth2AccessTokenConverter {
-}
--- src/main/java/com/example/oauth2/controller/AuthenticationController.java
+++
@@ -1,4 +0,0 @@
-package com.example.oauth2.controller;
-
-public class AuthenticationController {
-}
