9691cbb79731b69ec85f673cf7fe851622664eed
WINDOWS <fahrizalshofyanaziz@gmail.com>
2024-09-04 14:03:31 +0700

    add : changes project

---
+++ src/main/java/com/project/oauth/auth/AuthenticationUsingOauthApplication.java
@@ -0,0 +1,13 @@
+package com.project.oauth.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationUsingOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationUsingOauthApplication.class, args);
+	}
+
+}
